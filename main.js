122e0b59f15f8c9a2766b264abc61eb4929fc7e4
Masadavide <davide.masadm@gmail.com>
2022-01-10 15:59:08 +0100

    esercizio di base

--- main.js
+++ main.js
@@ -2,31 +2,55 @@ const team = [
   {
     name: 'Wayne Barnett',
     role: 'Founder & CEO',
-    image: 'wayne-barnett-founder-ceo.jpg',
+    image: 'img/wayne-barnett-founder-ceo.jpg',
   },
   {
     name: 'Angela Caroll',
     role: 'Chief Editor',
-    image: 'angela-caroll-chief-editor.jpg',
+    image: 'img/angela-caroll-chief-editor.jpg',
   },
   {
     name: 'Walter Gordon',
     role: 'Office Manager',
-    image: 'walter-gordon-office-manager.jpg',
+    image: 'img/walter-gordon-office-manager.jpg',
   },
   {
     name: 'Angela Lopez',
     role: 'Social Media Manager',
-    image: 'angela-lopez-social-media-manager.jpg',
+    image: 'img/angela-lopez-social-media-manager.jpg',
   },
   {
     name: 'Scott Estrada',
     role: 'Developer',
-    image: 'scott-estrada-developer.jpg',
+    image: 'img/scott-estrada-developer.jpg',
   },
   {
     name: 'Barbara Ramos',
     role: 'Graphic Designer',
-    image: 'barbara-ramos-graphic-designer.jpg',
+    image: 'img/barbara-ramos-graphic-designer.jpg',
   },
 ];
+
+console.log(team.length);
+let container = document.querySelector(".team-container");
+
+for (i = 0; i < team.length; i++){
+  creazioneTeamCard()
+}
+
+
+function creazioneTeamCard(){
+  container.innerHTML += `
+  <div class="team-card">
+  <div class="card-image">
+    <img
+      src="${team[i].image}"
+      alt=""
+    />
+  </div>
+  <div class="card-text">
+    <h3>${team[i].name}</h3>
+    <p>${team[i].role}</p>
+  </div>
+  `
+}
