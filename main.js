308c4b237ade5223e9128b1dca93f23bc1141524
Masadavide <davide.masadm@gmail.com>
2022-01-11 14:50:18 +0100

    finito

--- main.js
+++ main.js
@@ -34,23 +34,85 @@ const team = [
 console.log(team.length);
 let container = document.querySelector(".team-container");
 
+
+
+
+let btn = document.getElementById('addMemberButton');
+
+//esercizio di base
+
+
+
 for (i = 0; i < team.length; i++){
-  creazioneTeamCard()
+  creazioneTeamCard(team[i])
+}
+
+  function creazioneTeamCard(){
+    container.innerHTML += `
+    <div class="team-card">
+    <div class="card-image">
+    <img
+    src="${team[i].image}"
+    alt=""
+    />
+    </div>
+    <div class="card-text">
+    <h3>${team[i].name}</h3>
+    <p>${team[i].role}</p>
+    </div>
+    `
 }
 
 
-function creazioneTeamCard(){
+//Aggiungere membri creati dall'utente da tasto add
+
+
+
+
+function creazioneTeamCard2(){
+  let nome = document.getElementById('name').value;
+  let role = document.getElementById('role').value;
+  let image = document.getElementById('image').value;
+
+  svuotaForm();
+
+  //oggetto da pushare nell'arrey
+  let userCard ={
+    nome : nome,
+    role : role,
+    image : image,
+  }
+
   container.innerHTML += `
   <div class="team-card">
   <div class="card-image">
     <img
-      src="${team[i].image}"
+      src="${image}"
       alt=""
     />
   </div>
   <div class="card-text">
-    <h3>${team[i].name}</h3>
-    <p>${team[i].role}</p>
+    <h3>${nome}</h3>
+    <p>${role}</p>
   </div>
   `
+  //pushiamo l'oggetto nel nostro array e vediamo se viene aggiunto
+  team.push(userCard);
+  console.log(team);
 }
+
+
+function svuotaForm(){
+  document.getElementById("name").value = "";
+  document.getElementById("role").value = "";
+  document.getElementById("image").value = "" ;
+}
+
+
+btn.addEventListener("click", creazioneTeamCard2);
+
+
+
+
+
+
